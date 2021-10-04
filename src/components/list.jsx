d619639dacbc098326e5671ac6d20e0abac05cc7
Bricen Bennett <bricen@email.sc.edu>
2021-10-04 17:09:30 -0400

    first commit

---
+++ src/components/list.jsx
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+
+class List extends React.Component{
+
+    handleChange(e) {
+        this.setState({text: e.target.value});
+    }
+    /** Just to see if anything was typed in */
+    handleSubmit(e) {
+        e.preventDefault();
+        if(this.state.text.length === 0) {
+            return;
+        }
+        const newItem = {
+            /** saves the text to add to list */
+            text: this.state.text,
+            id: Date.now()
+        };
+        this.setState(state => ({
+            items: state.items.concat(newItem),
+            text: ''
+        }));
+    }
+    constructor(props) {
+        super(props);
+        this.state = { items: [], text: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+
+    render () {
+        return (
+            <div>
+               <h3>Shopping List</h3>
+               <TodoList items={this.state.items}/>
+               <form onSubmit={this.handleSubmit}>
+                   <label htmlFor="new-todo">
+                       TO BUY
+                   </label>
+                   <input
+                        id="new-todo"
+                        onChange={this.handleChange}
+                        value={this.state.text}
+                    />
+                    <button>
+                        Add {this.state.items.length + 1}
+                    </button>
+                </form>
+            </div>
+        );
+    }
+   
+}
+class TodoList extends React.Component {
+    render() {
+        return(
+            <ol>
+                {this.props.items.map(item => (
+                    <li key={item.id}>{item.text}</li>
+                ))}
+            </ol>
+        );
+    }
+}
+
+export default List;
