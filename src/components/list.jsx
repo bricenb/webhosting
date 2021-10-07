70d843045e5aa5e432f2ccf38797c58a203c6105
Bricen Bennett <bricen@email.sc.edu>
2021-10-07 15:18:35 -0400

    css

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
+import logo from "./shoppingcart.png";
 
 
 class List extends React.Component{
 
-    handleChange(e) {
-        this.setState({text: e.target.value});
+
+    constructor() {
+        super();
+        this.state = { items: [], text: ''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({text: event.target.value});
     }
     /** Just to see if anything was typed in */
-    handleSubmit(e) {
-        e.preventDefault();
+    handleSubmit(event) {
+        event.preventDefault();
         if(this.state.text.length === 0) {
             return;
         }
@@ -22,23 +31,16 @@ class List extends React.Component{
             text: ''
         }));
        
-    }
-    constructor(props) {
-        super(props);
-        this.state = { items: [], text: ''};
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    
-    
-    
+    } 
 
     render () {
         return (
-            <div>
-               <h3>Shopping List</h3>
-               <TodoList items={this.state.items}/>
+            <html>
+            <header>
+               <img src={logo}/>Shopping List<img src={logo}/>
+            </header>
+            <body>
+               <h4>{this.state.items.length} Different Items on List</h4>
                <form onSubmit={this.handleSubmit}>
                    <label htmlFor="new-todo">
                        TO BUY
@@ -52,7 +54,16 @@ class List extends React.Component{
                         Add {this.state.items.length + 1}
                     </button>
                 </form>
-            </div>
+                <div>
+                <button onClick={() => window.location.reload(false)}>CLEAR LIST</button>
+                </div>
+                <TodoList items={this.state.items}/>
+                </body>
+            
+            </html>
+
+
+
         );
     }
    
@@ -64,9 +75,53 @@ class TodoList extends React.Component {
         return(
             <ol>
                 {this.props.items.map(item => (
-                    <li key={item.id}>{item.text}<input type="checkbox"></input></li>
+                    <li key={item.id}>{item.text}<input type="checkbox"></input><Button /></li>
                 ))}
             </ol>
+        );
+    }
+}
+
+class ButtonClick extends React.Component {
+    render () {
+        let {title,action} = this.props
+        return (
+            <button onClick = {action}>{title}</button>
+        );
+    }
+}
+
+class Button extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+          count: 1,
+        };
+      }
+    
+      incrementCount = () => {
+        this.setState({
+          count: this.state.count + 1
+        });
+      };
+    
+      decrementCount = () => {
+          if (this.state.count === 1) {
+              return;
+          }
+        this.setState({
+          count: this.state.count - 1
+        });
+      };
+
+    render() {
+        let { count } = this.state;
+        return (
+                <button>
+                <h4>{count}</h4>
+                <ButtonClick title={"-"} action = {this.decrementCount} />
+                <ButtonClick title={"+"} action = {this.incrementCount}/>
+                </button>
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import 'bootstrap/dist/css/bootstrap.css';
 import List from './components/list';
-import Dropdow from './components/dropdow';
 
 ReactDOM.render(
   <React.StrictMode>
